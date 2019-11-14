f762d264f43db683ffc4202442c4cf18470d3f86
Vlusion <vicktopmail@gmail.com>
2019-11-14 14:37:35 +0100

    Login pagina V2.1 (Login heeft nu eigen component en wordt gelijk gestart) Nu echt

---
+++ src/app/inloggen/inloggen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inloggen',
+  templateUrl: './inloggen.component.html',
+  styleUrls: ['./inloggen.component.css']
+})
+export class InloggenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
