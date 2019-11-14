f9c06013faf144a518ba77a57775a96e538c20bd
Vlusion <vicktopmail@gmail.com>
2019-11-14 15:23:35 +0100

    Transactieoverzicht V1 Basis geimplementeerd.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {OverzichtComponent} from './overzicht/overzicht.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',     component: OverzichtComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
