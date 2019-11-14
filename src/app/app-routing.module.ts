127d40b05e0baadc7ec4642a7ee8fb4c44426928
Vlusion <vicktopmail@gmail.com>
2019-11-14 14:34:53 +0100

    Login pagina V2 (Login heeft nu eigen component en wordt gelijk gestart)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {InloggenComponent} from './inloggen/inloggen.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',     component: InloggenComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InloggenComponent } from './inloggen/inloggen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InloggenComponent
   ],
   imports: [
     BrowserModule,
