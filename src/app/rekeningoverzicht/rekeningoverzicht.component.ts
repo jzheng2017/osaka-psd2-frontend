040b42df87ae68484fe90295d4ee3fa16c3c7157
Vlusion <vicktopmail@gmail.com>
2019-11-15 11:08:14 +0100

    Rekeningoverzicht REVERT V2 Nu juiste bestanden toegevoegd

---
+++ src/app/rekeningoverzicht/rekeningoverzicht.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rekeningoverzicht',
+  templateUrl: './rekeningoverzicht.component.html',
+  styleUrls: ['./rekeningoverzicht.component.css']
+})
+export class RekeningoverzichtComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
