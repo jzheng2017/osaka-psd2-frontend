4314f5454a9f3592ad5d0e27f0e26654906a223b
Vlusion <vicktopmail@gmail.com>
2019-11-15 11:18:07 +0100

    Transactieoverzicht V1 Basis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TransactieoverzichtComponent } from './transactieoverzicht/transactieoverzicht.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransactieoverzichtComponent
   ],
   imports: [
     BrowserModule,
