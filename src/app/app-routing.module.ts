6b50bfc101f370440247e87dd7290cfd4cfdda74
Vlusion <vicktopmail@gmail.com>
2019-11-15 16:10:49 +0100

    Routing nu: AppComponent InloggenComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AppComponent} from './app.component';
+import {InloggenComponent} from './inloggen/inloggen.component';
 
 
 const routes: Routes = [
-  { path: '',     component: AppComponent}
+  { path: '',     component: AppComponent},
+  { path: 'inloggen',     component: InloggenComponent}
 ];
 
 @NgModule({
