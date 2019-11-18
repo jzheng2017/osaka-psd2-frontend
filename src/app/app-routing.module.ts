25cdd6a384fa9b9deffd2fd903eebb8a631a643d
Jiankai Zheng <J.Zheng1@student.han.nl>
2019-11-18 10:21:05 +0100

    router link fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {AppComponent} from './app.component';
 import {InloggenComponent} from './inloggen/inloggen.component';
 import {RegistrationComponent} from './registration/registration.component';
 
 
 const routes: Routes = [
-    {path: '', component: AppComponent},
     {path: 'registration', component: RegistrationComponent},
     {path: 'inloggen', component: InloggenComponent}
 ];
