493c454f5c9332041870b665a30e47c8effc77a5
Jiankai Zheng <J.Zheng1@student.han.nl>
2019-11-18 15:36:25 +0100

    login validation

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {Registration} from '../registration';
-import {Router} from "@angular/router";
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-registration',
