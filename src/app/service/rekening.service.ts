e0c764ac99e947b26fbfd8bffd07ef0af25ac78c
Jiankai Zheng <J.Zheng1@student.han.nl>
2019-11-26 10:41:13 +0100

    get token for api url

--- src/app/service/rekening.service.ts
+++ src/app/service/rekening.service.ts
@@ -7,10 +7,11 @@ import {Rekening} from '../rekening/rekening';
     providedIn: 'root'
 })
 export class RekeningService {
-    private apiUrl = 'http://localhost:8080/rabo/accounts?token=AAIkYzQ1MTc3OGMtZGIyYy00NTFlLThmNTctOWJiNjI0MjIzMjlldkdshTai250biX71DAtcAZQXRE6vmyvfRAIDpT4FNBzXjs39YdUcCiQ6sZGXGCBXM_jF9YRYKL-cyUOvBfcg12ssb-70ZR9LGN7tfDgOsNrtp4fmq3UNpY8lr-gHkmPiyOhMYz5w6dZDIZmZpZQIfSxate6JTNb1ouhazcU80eApZxlGcsoC9pM45Bu8K2MKLMcMiazraJuG6Uonn-vtgIU4Yn1mtJ_2tJXTJVqCNF4';
-
+    private token = localStorage.getItem('token');
+    private apiUrl = `http://localhost:8080/rabo/accounts?token=${this.token}`;
     constructor(private http: HttpClient) {
     }
+
 
     getRekeningen(): Observable<Rekening[]> {
         return this.http.get<Rekening[]>(this.apiUrl);
