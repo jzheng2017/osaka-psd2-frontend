c411eb48233f48ed3bed4cc88110641318784c26
Jiankai Zheng <J.Zheng1@student.han.nl>
2019-11-27 12:33:39 +0100

    protected basic routes when not authorized with guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,15 +6,16 @@ import {RekeningoverzichtComponent} from './rekeningoverzicht/rekeningoverzicht.
 import {RekeningComponent} from './rekening/rekening.component';
 import {TransactionComponent} from './transaction/transaction.component';
 import {AddBankComponent} from './add-bank/add-bank.component';
+import {AuthGuardService} from './service/auth-guard.service';
 
 
 const routes: Routes = [
     {path: 'registration', component: RegistrationComponent},
     {path: 'login', component: InloggenComponent},
-    {path: 'overzicht/rekeningen', component: RekeningoverzichtComponent},
-    {path: 'rekening/:id/details', component: RekeningComponent},
-    {path: 'transactie/:id/details', component: TransactionComponent},
-    {path: 'bank-koppelen', component: AddBankComponent},
+    {path: 'overzicht/rekeningen', component: RekeningoverzichtComponent, canActivate: [AuthGuardService]},
+    {path: 'rekening/:id/details', component: RekeningComponent, canActivate: [AuthGuardService]},
+    {path: 'transactie/:id/details', component: TransactionComponent, canActivate: [AuthGuardService]},
+    {path: 'bank-koppelen', component: AddBankComponent, canActivate: [AuthGuardService]},
     { path: '**', redirectTo: 'overzicht/rekeningen', pathMatch: 'full' },
 ];
 
