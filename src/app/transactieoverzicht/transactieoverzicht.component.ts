36690052dd07f6cba19f5001db22741eb29c2909
Jiankai Zheng <J.Zheng1@student.han.nl>
2019-12-02 10:25:01 +0100

    refactor transactieoverzicht component

--- src/app/transactieoverzicht/transactieoverzicht.component.ts
+++ src/app/transactieoverzicht/transactieoverzicht.component.ts
@@ -4,28 +4,31 @@ import {Transaction} from '../transaction/dto/transaction';
 import {ActivatedRoute} from '@angular/router';
 
 @Component({
-    selector: 'app-transactieoverzicht',
-    templateUrl: './transactieoverzicht.component.html',
-    styleUrls: ['./transactieoverzicht.component.css']
+  selector: 'app-transactieoverzicht',
+  templateUrl: './transactieoverzicht.component.html',
+  styleUrls: ['./transactieoverzicht.component.css']
 })
 export class TransactieoverzichtComponent implements OnInit {
 
-    transactions: Transaction[];
-    isLoading = true;
+  transactions: Transaction[];
+  isLoading = true;
 
-    constructor(private transactionService: TransactionService, private activatedRoute: ActivatedRoute) {
-    }
+  constructor(private transactionService: TransactionService, private activatedRoute: ActivatedRoute) {
+  }
 
-    ngOnInit() {
-        this.getTransactions();
-    }
+  ngOnInit() {
+    this.getTransactions();
+  }
 
-    getTransactions() {
-        this.transactionService.getTransacties(this.activatedRoute.snapshot.paramMap.get('id'), this.activatedRoute.snapshot.paramMap.get('tableid')).subscribe(transactions => {
-            this.transactions = transactions.transactions;
-            this.isLoading = false;
-        });
-    }
+  getTransactions() {
+    const bankAccountId = this.activatedRoute.snapshot.paramMap.get('id');
+    const tableId = this.activatedRoute.snapshot.paramMap.get('tableid');
+
+    this.transactionService.getTransacties(bankAccountId, tableId).subscribe(transactions => {
+      this.transactions = transactions.transactions;
+      this.isLoading = false;
+    });
+  }
 
 
 }
