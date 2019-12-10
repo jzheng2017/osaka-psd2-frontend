7d9a0ae7a10f947dab40a95fc39b20cd57291445
Jiankai Zheng <J.Zheng1@student.han.nl>
2019-12-10 09:20:12 +0100

    refactor api url

--- src/app/service/users/user.service.ts
+++ src/app/service/users/user.service.ts
@@ -4,37 +4,36 @@ import {HttpClient} from '@angular/common/http';
 import {GetUserRequest} from '../../login/dto/getuserrequest';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class UserService {
 
   private getUserRequest: GetUserRequest;
   private apiUrl = 'http://steinmilder.nl:8080';
 
-    constructor(private httpClient: HttpClient, private router: Router) {
-    }
+  constructor(private httpClient: HttpClient, private router: Router) {
+  }
 
   public getUser() {
-    const getUserUrl = this.apiUrl + '/users/user/details?token=' + localStorage.getItem('token');
     const token = localStorage.getItem('token');
+    const getUserUrl = `${this.apiUrl}/users/user/details?token=${token}`;
     return this.httpClient.get<any>(getUserUrl);
   }
 
-  public getAttachedBankAccounts()  {
-      const getAttachedBankAccountsUrl = this.apiUrl + '/users/user/attachedaccounts?token=' + localStorage.getItem('token');
-      const token = localStorage.getItem('token');
-      return this.httpClient.get<any>(getAttachedBankAccountsUrl);
+  public getAttachedBankAccounts() {
+    const token = localStorage.getItem('token');
+    const getAttachedBankAccountsUrl = `${this.apiUrl}/users/user/attachedaccounts?token=${token}`;
+    return this.httpClient.get<any>(getAttachedBankAccountsUrl);
   }
 
   public disconnectBankAccount(id: number) {
-      const url = 'http://steinmilder.nl:8080/disconnect';
-      const token = localStorage.getItem('token');
-      return this.httpClient.delete(url + `/?token=${token}&tableid=${id}`);
+    const token = localStorage.getItem('token');
+    const url = `${this.apiUrl}/disconnect?token=${token}&tableid=${id}`;
+    return this.httpClient.delete(url);
   }
 
-
-    logout() {
-        localStorage.removeItem('token');
-    }
+  logout() {
+    localStorage.removeItem('token');
+  }
 }
 
