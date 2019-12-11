dfb43fb8d477476cb0b6367a4aa28a4a74b8c68b
Jiankai Zheng <J.Zheng1@student.han.nl>
2019-12-11 13:41:58 +0100

    move categoryService

--- src/app/service/category/category.service.ts
+++ src/app/service/category/category.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {ConfigService} from './service/config/config.service';
+import {ConfigService} from '../config/config.service';
 
 @Injectable({
   providedIn: 'root'
