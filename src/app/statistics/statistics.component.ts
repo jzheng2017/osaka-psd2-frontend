9de73a6631126fa17053eb83e07da234a848b713
Jiankai Zheng <J.Zheng1@student.han.nl>
2019-12-16 15:55:03 +0100

    refactor: second chart type to ColumnChart

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -18,7 +18,7 @@ export class StatisticsComponent implements OnInit {
       ['12-05-2019', 1],
       ['12-06-2019', 2],
       ['12-07-2019', 3],
-    ], new ChartSettings('Transacties Taart grafiek', 500, true, 'PieChart'))
+    ], new ChartSettings('Transacties Taart grafiek', 500, true, 'ColumnChart'))
   ];
 
   constructor() {
