ef0b374646cc8d4ecefc43033d3e8a69a06b34b5
Jiankai Zheng <J.Zheng1@student.han.nl>
2019-12-17 10:03:03 +0100

    add: statistic li item to action items

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,6 +26,7 @@ const routes: Routes = [
     {path: 'overmaken', component: TransferComponent, canActivate: [AuthGuardService]},
     {path: 'instellingen', component: InstellingenComponent, canActivate: [AuthGuardService]},
     {path: 'statistieken', component: StatisticsComponent, canActivate: [AuthGuardService]},
+    {path: 'statistieken/:id/tableId/:tableid', component: StatisticsComponent, canActivate: [AuthGuardService]},
     {path: '**', redirectTo: 'overzicht/rekeningen', pathMatch: 'full'},
 ];
 
