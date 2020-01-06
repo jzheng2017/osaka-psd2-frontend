195c52ec9e0adeaba76c17e97700e33e2bf88138
Jiankai Zheng <jzheng.1@student.han.nl>
2020-01-06 11:27:02 +0100

    add: message when payment succesfully transfered

--- src/app/transfer/transfer.component.ts
+++ src/app/transfer/transfer.component.ts
@@ -4,6 +4,7 @@ import {Rekening} from '../rekening/dto/rekening';
 import {RekeningService} from '../service/banks/rekening.service';
 import {Location} from '@angular/common';
 import {TransactionService} from '../service/banks/transaction.service';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-transfer',
@@ -18,10 +19,13 @@ export class TransferComponent implements OnInit {
   isLoading = true;
   transfered = undefined;
 
-  constructor(private rekeningService: RekeningService, private transactionService: TransactionService, private location: Location) {
+  constructor(private rekeningService: RekeningService, private transactionService: TransactionService, private location: Location, private route: ActivatedRoute) {
   }
 
   ngOnInit() {
+    if (this.route.snapshot.queryParamMap.get('success') === 'true') {
+      this.transfered = true;
+    }
     this.getRekeningen();
   }
 
