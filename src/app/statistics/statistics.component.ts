fbc2b6cd46c5d5c80e89207e0dc67f7826032337
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-01-08 10:42:42 +0100

    add: new chart & function for grouping transactions by sender

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -3,7 +3,7 @@ import {Chart} from './dto/chart';
 import {TransactionService} from '../service/banks/transaction.service';
 import {ActivatedRoute} from '@angular/router';
 import {ChartSettings} from './dto/chart-settings';
-import {Location} from "@angular/common";
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-statistics',
@@ -11,7 +11,12 @@ import {Location} from "@angular/common";
   styleUrls: ['./statistics.component.css']
 })
 export class StatisticsComponent implements OnInit {
-  charts: Chart[] = [new Chart([], new ChartSettings('Aantal transacties per dag (minimaal 1 transactie)', 100, true, 'LineChart'))];
+  charts: Chart[] = [
+    new Chart([], new ChartSettings('Aantal transacties per dag (minimaal 1 transactie)',
+      100, true, 'LineChart')),
+    new Chart([], new ChartSettings('Aantal transacties per tegenrekening (minimaal 1 transactie)',
+      100, true, 'PieChart'))
+  ];
   isLoading = true;
 
   constructor(private transactionService: TransactionService, private activatedRoute: ActivatedRoute, private location: Location) {
@@ -27,17 +32,36 @@ export class StatisticsComponent implements OnInit {
     this.transactionService.getTransactions(accountId, tableId).subscribe(transactions => {
       this.isLoading = false;
       this.charts[0].data = this.groupTransactionsByDate(transactions.transactions);
+      this.charts[1].data = this.groupTransactionsBySender(transactions.transactions);
     }, () => {
       this.isLoading = undefined;
     });
   }
 
   groupTransactionsByDate(arr: any) {
+    return this.groupTransactionBy('date', arr);
+  }
+
+  groupTransactionsBySender(arr: any) {
+    return this.groupTransactionBy('iban', arr);
+  }
+
+  // grootste spaghetti code dat ik ooit heb geschreven, maar het werkt
+  groupTransactionBy(type: string, arr: any) {
     if (arr === null || arr === undefined) {
       return null;
     }
     console.log(arr);
-    let dateArray = arr.map(entity => entity.date);
+    let dateArray;
+    let text;
+    if (type === 'date') {
+      text = 'Onbekende datum';
+      dateArray = arr.map(entity => entity.date);
+    } else if (type === 'iban') {
+      arr = arr.filter(entity => entity.received === true);
+      text = 'Onbekende rekening';
+      dateArray = arr.map(entity => entity.sender.iban);
+    }
     let sum = [];
     dateArray.some((entity) => {
       let found = false;
@@ -52,7 +76,7 @@ export class StatisticsComponent implements OnInit {
       });
       if (!found) {
         if (!entity) {
-          entity = 'Onbekende datum';
+          entity = text;
         }
         sum.push([entity, 1]);
       }
